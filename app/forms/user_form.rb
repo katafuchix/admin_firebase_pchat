16feae7a20b78e2157c0a7f0ca9c4b8fa11e1fef
katafuchix <katafuchix@gmail.com>
2021-04-24 02:32:25 +0900

    prefecture select sample

--- app/forms/user_form.rb
+++ app/forms/user_form.rb
@@ -5,10 +5,14 @@ class UserForm
   attribute :id, String
   attribute :nickname, String
   attribute :profile_text, String
+  attribute :status, Int = 0
+
+  STATUS = { ok: true, ng: false }.with_indifferent_access
 
   validates_presence_of %i(
     id
     nickname
     profile_text
+    status
   )
 end
